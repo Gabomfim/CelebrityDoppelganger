625f3fa017fc46bcaa7c3e707ca7a26e36629670
Gabomfim <gabomfim99@gmail.com>
2024-12-28 14:33:55 -0300

    First layout

--- my-flask-app/app/__init__.py
+++ my-flask-app/app/__init__.py
@@ -2,12 +2,6 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    
-    # Load configuration
-    app.config.from_mapping(
-        SECRET_KEY='your_secret_key',
-        DATABASE='your_database_path'
-    )
 
     # Register blueprints
     from . import routes
